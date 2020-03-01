e90b2813ef2d842930f377dc87ab22eeda771d62
Vl3Dick <kisonika228@gmail.com>
2020-03-01 19:19:34 +0200

    you can now run the app

--- src/redux/root-reducer.ts
+++ src/redux/root-reducer.ts
@@ -19,4 +19,4 @@ export type AppState = ReturnType<typeof reducers>;
 
 
 // @ts-ignore: Unreachable code error
-export default createStore(reducers, compose(applyMiddleware(...middleware), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()));
+export default createStore(reducers, compose(applyMiddleware(...middleware)));
